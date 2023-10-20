d9a72f7ea17b4f06fab7ccb70c6da7c1e2c6030a
aamandaericson <>
2023-10-20 08:49:21 +0000

    Fix Typo.

--- todo/models.py
+++ todo/models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    done = models.BooleanField(null=False, blank=False default=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
